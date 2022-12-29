f642067fd4201bdc3060b6b16404de26fbfc2308
LDG33 <118735787+LDG33@users.noreply.github.com>
2022-12-29 18:26:12 +0000

    comment message
    LDG 20221229

---
+++ lorem.php
@@ -0,0 +1,14 @@
+<?php
+
+$x=5;
+while($x>0)
+{
+echo("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis obcaecati laboriosam tempore
+ expedita neque doloremque explicabo optio maiores commodi laborum?");
+
+$x--;
+}
+
+// comment message
+
+?>
